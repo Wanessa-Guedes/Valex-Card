6d6de86315d8837e558176da2c5097b4149a0674
Wanessa de Oliveira Guedes <BETHA\wanessa.guedes@fyr84t3.betha.com.br>
2023-02-22 11:31:31 -0300

    Virtual-Card-DELETE-AND-TRANSACTION

--- src/main/java/com/api/valex/Controllers/VirtualCardController.java
+++ src/main/java/com/api/valex/Controllers/VirtualCardController.java
@@ -1,15 +1,13 @@
 package com.api.valex.Controllers;
 
+import com.api.valex.Controllers.dto.PaymentCardDto;
 import com.api.valex.Controllers.dto.VirtualCardCreateDto;
 import com.api.valex.Middlewares.ErrorHandler400;
 import com.api.valex.Middlewares.ErrorHandler404;
 import com.api.valex.Services.VirtualCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/virtual")
@@ -20,6 +18,18 @@ public class VirtualCardController {
     @PostMapping("/create")
     public ResponseEntity<Object> CreateVirtualCard(@RequestBody VirtualCardCreateDto req) throws ErrorHandler404, ErrorHandler400 {
         virtualCardService.CreateVirtual(req);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Object> DeleteVirtualCard(@RequestBody VirtualCardCreateDto req) throws ErrorHandler404, ErrorHandler400 {
+        virtualCardService.DeleteVirtual(req);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/payment")
+    public ResponseEntity<Object> TransactionVirtual(@RequestBody PaymentCardDto req) throws ErrorHandler404, ErrorHandler400 {
+        virtualCardService.VirtualTransaction(req);
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/api/valex/Services/VirtualCardService.java
+++ src/main/java/com/api/valex/Services/VirtualCardService.java
@@ -1,16 +1,25 @@
 package com.api.valex.Services;
 
+import com.api.valex.Controllers.dto.PaymentCardDto;
 import com.api.valex.Controllers.dto.VirtualCardCreateDto;
 import com.api.valex.Middlewares.ErrorHandler400;
 import com.api.valex.Middlewares.ErrorHandler404;
+import com.api.valex.Models.Businesses;
 import com.api.valex.Models.Cards;
+import com.api.valex.Models.Payments;
+import com.api.valex.Models.Recharges;
+import com.api.valex.Repositories.BusinessesRepository;
 import com.api.valex.Repositories.CardRepository;
+import com.api.valex.Repositories.PaymentRepository;
+import com.api.valex.Repositories.RechargesRepository;
 import com.github.javafaker.CreditCardType;
 import com.github.javafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static com.api.valex.Middlewares.Crypt.crypt;
@@ -21,6 +30,15 @@ public class VirtualCardService {
     @Autowired
     CardRepository cardRepository;
 
+    @Autowired
+    BusinessesRepository businessesRepository;
+
+    @Autowired
+    RechargesRepository rechargesRepository;
+
+    @Autowired
+    PaymentRepository paymentRepository;
+
     public void CreateVirtual(VirtualCardCreateDto req) throws ErrorHandler400, ErrorHandler404 {
         Cards card = cardRepository.findBySecurityCode(req.getSecurityCode());
         if(card == null){
@@ -29,6 +47,9 @@ public class VirtualCardService {
 
         if(card.getPassword() == null){
             throw new ErrorHandler400("400", "Cartão não ativado");
+        }
+        if(card.getBlocked()){
+            throw new ErrorHandler400("400", "Cartão Bloqueado");
         }
         String cryptPass = crypt(req.getPassword());
         if(!card.getPassword().equals(cryptPass)){
@@ -50,6 +71,80 @@ public class VirtualCardService {
         virtualCard.setBlocked(false);
         virtualCard.setType(card.getType());
         virtualCard.setPassword(cryptPass);
+        virtualCard.setCard(card);
         cardRepository.save(virtualCard);
+    }
+
+    public void DeleteVirtual(VirtualCardCreateDto req) throws ErrorHandler404, ErrorHandler400 {
+        Cards card = cardRepository.findBySecurityCode(req.getSecurityCode());
+        if(card == null){
+            throw new ErrorHandler404("404", "Cartão não encontrado");
+        }
+        if(!card.getVirtual()){
+            throw new ErrorHandler400("400", "Cartão não é virtual");
+        }
+        String cryptPass = crypt(req.getPassword());
+        if(!card.getPassword().equals(cryptPass)){
+            throw new ErrorHandler400("400", "Senha incorreta");
+        }
+
+        cardRepository.delete(card);
+
+    }
+
+    public void VirtualTransaction(PaymentCardDto req) throws ErrorHandler400, ErrorHandler404 {
+        if(req.getAmount() <= 0){
+            throw new ErrorHandler400("400", "Valor da compra deve ser maior do que zero");
+        }
+        Cards card = cardRepository.findById(req.getCardId());
+        if(card == null){
+            throw new ErrorHandler404("404", "Cartão não encontrado");
+        }
+
+        LocalDate todayDate = LocalDate.now();
+        if (todayDate.isAfter(card.getExpirationDate())) {
+            throw new ErrorHandler400("400", "Cartão expirado");
+        }
+
+        if(card.getBlocked()){
+            throw new ErrorHandler400("400", "Cartão bloqueado");
+        }
+
+        String cryptPass = crypt(req.getPassword());
+        if(!card.getPassword().equals(cryptPass)){
+            throw new ErrorHandler400("400", "Senha incorreta");
+        }
+
+        Businesses business = businessesRepository.findById(req.getBusinessId());
+        if(business == null){
+            throw new ErrorHandler400("400", "Estabelecimento não cadastrado");
+        }
+
+        if(card.getType() != business.getType()){
+            throw new ErrorHandler400("400", "Tipo de transação incompatível");
+        }
+
+        List<Recharges> recharges = rechargesRepository.findAllByCardId(card.getCard().getId());
+        List<Payments> payments = paymentRepository.findAllByCardId(card.getCard().getId());
+        Float totalRecharge = 0.0F;
+        for(Recharges r : recharges){
+            totalRecharge += r.getAmount();
+        }
+        Float totalPayments = 0.0F;
+        for(Payments p : payments){
+            totalPayments += p.getAmount();
+        }
+        Float balance = totalRecharge - totalPayments - req.getAmount();
+        Payments payment = new Payments();
+        payment.setCard(card.getCard());
+        payment.setAmount(req.getAmount());
+        payment.setBusiness(business);
+        payment.setTimestamp(Timestamp.valueOf(LocalDate.now().atStartOfDay()));
+        if(balance>0){
+            paymentRepository.save(payment);
+        } else {
+            throw new ErrorHandler400("400", "Não há fundos suficientes para completar a transação");
+        }
+
     }
 }
